c7667f2c8b6152674873c40019b9ca1df773e8d9
Damião Almeida <damiao.almeida@ifpb.edu.br>
2025-05-06 17:22:51 -0300

    Classe livro

---
+++ src/cadastro/Livro.java
@@ -0,0 +1,23 @@
+package cadastro;
+
+public class Livro {
+	private Long id;
+	
+	private String titulo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+}
