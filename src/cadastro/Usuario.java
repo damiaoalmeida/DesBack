9756f933730ba236f291edc759228880f1217ea9
damiao <damiao.almeida@ifpb.edu.br>
2026-08-21 00:29:18 -0300

    bancoEmMemoria

--- src/cadastro/Usuario.java
+++ src/cadastro/Usuario.java
@@ -1,5 +1,35 @@
 package cadastro;
 
-public class Usuario {
+import java.util.Date;
 
+public class Usuario {
+	private int id;
+	private String nome;
+	private String email;
+	private Date dataNascimento;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 }
